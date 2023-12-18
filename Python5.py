f1a9064d0b470bc1354831bc166817f58f13eb9c
mileenamury <vibesstudy2@gmail.com>
2023-12-18 09:30:06 -0300

    While

---
+++ Python5.py
@@ -0,0 +1,31 @@
+# estrutura de repetição while, será executado enquanto a condição for verdadeira
+
+x = 1
+
+while x <= 3:
+
+    print(x)
+
+    x = x + 1
+print("Números de 1 até 100")
+# Não exibirá 3 + 1, uma vez que 4 demonstra que a condição é False
+
+y = 1
+
+while y <= 100:
+
+    print(y)
+
+    y = y + 1
+
+#####################
+print("Número de 50 até 100")
+
+z = 50
+
+while z <= 100:
+    
+    print(z)
+
+    z = z + 1
+
